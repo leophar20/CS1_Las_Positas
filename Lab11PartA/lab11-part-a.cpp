a27f14d7018a79d80ccb42c38b19b9b17ef6363e
leoph <leoph@Raphaels-Driver>
2021-09-30 09:35:01 -0700

    lab11 part A

---
+++ Lab11PartA/lab11-part-a.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // variables
+    int maxVal = 0, userNumber = 0;
+    const int SENTINEL = -1;
+
+    // TODO: create a `while` loop version of finding the maximum number
+//    while (userNumber!= SENTINEL)
+//    {
+//    	cout << "Enter a number: ";
+//    	cin >> userNumber;
+//    	if (userNumber > maxVal )
+//    	{
+//    	  maxVal = userNumber;
+//    	}
+//    }
+    // TODO: create a `do-while` loop version of finding the maximum number
+//    do{
+//    	cout << "Enter a number: ";
+//    	cin >> userNumber;
+//    	 if (userNumber > maxVal )
+//    	 {
+//    	    maxVal = userNumber;
+//    	 }
+//    }
+//    while (userNumber!= SENTINEL);
+
+    // TODO: create a `for` loop version of finding the maximum number
+    for(;userNumber != SENTINEL;cin >> userNumber)
+    {
+    	cout << "Enter a number: ";
+    	if (userNumber > maxVal )
+    	{
+    	maxVal = userNumber;
+    	}
+    }
+
+    // display results
+    cout << "\nMaximum number entered: " << maxVal << endl;
+
+    // terminate
+    return 0;
+}
